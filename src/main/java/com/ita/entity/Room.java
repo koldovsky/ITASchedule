4957ef7cb7ade4c23d93b563207f45017f6e92eb
matsoRuslan <rulya.skillerone@gmail.com>
2016-09-20 20:05:10 +0300

    commit for bodya

---
+++ src/main/java/com/ita/entity/Room.java
@@ -0,0 +1,32 @@
+package com.ita.entity;
+
+/**
+ * Created by Skiller on 20.09.2016.
+ */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Room {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private int number;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
