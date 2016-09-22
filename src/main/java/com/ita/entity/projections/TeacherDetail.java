c203eb3f76eb32372387e0deeff15f94ff7546a6
levkovych67 <fnx@ukr.net>
2016-09-22 23:54:23 +0300

    teacher projection added

---
+++ src/main/java/com/ita/entity/projections/TeacherDetail.java
@@ -0,0 +1,20 @@
+package com.ita.entity.projections;
+
+
+import com.ita.entity.Event;
+import com.ita.entity.Ita_group;
+import com.ita.entity.Permission;
+import com.ita.entity.Teacher;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.List;
+
+@Projection(name = "detailed",types = {Teacher.class})
+public interface TeacherDetail {
+    String getFullName();
+    Boolean getActive();
+    String getContactInfo();
+    List<Ita_group> getGroups();
+    List<Event> getEvents();
+    List<Permission> getPermissions();
+}
