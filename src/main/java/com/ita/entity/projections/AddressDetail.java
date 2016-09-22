127603401c7f737a504edc519f847d1c96cd8d98
dimabush <dim.bush.ua@gmail.com>
2016-09-22 21:01:22 +0300

    event type repository

--- src/main/java/com/ita/entity/projections/AddressDetail.java
+++ src/main/java/com/ita/entity/projections/AddressDetail.java
@@ -6,10 +6,16 @@ import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "AddressDetail", types = {Address.class})
 public interface AddressDetail {
+
     String getAddress();
+
     String getCodeName();
+
     String getWorkingHoursStart();
+
     String getWorkingHoursEnd();
+
     Boolean getActive();
+
     City getCity();
 }
