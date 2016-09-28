f9e8fa687e87d1cc5d83aca5fdbc99d0874b7a77
seliuchenko <m.seliuchenko@gmail.com>
2016-09-28 18:53:56 +0300

    Removed old files

--- src/main/java/com/ita/config/JpaConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.ita.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = ("com.ita.repository"))
-public class JpaConfiguration {
-}
