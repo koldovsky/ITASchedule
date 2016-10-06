d7b6b3749e4e52b0afb60b55856cecc8dec3a238
Sviatko <sviat.dub@gmail.com>
2016-10-06 19:56:32 +0300

    sviatko 3 files added

---
+++ Client/src/client/app/teachers/teacher.controller.js
@@ -0,0 +1,61 @@
+(function() {
+'use strict';
+    angular
+        .module('app.administrator.teachers')
+        .controller('teachersController', teachersController);
+
+    teachersController.$inject = ['$q', 'teacherservice', 'logger','$scope', '$stateParams', '$state' ];
+    function teachersController($q, teacherservice, logger, $scope, $stateParams,$state) {
+        var vm = this;
+        vm.roleName=$stateParams.params.roleName;
+        vm.teacher={};
+        vm.teachers=[];
+        vm.displayedTeachers=[];
+        vm.state=$stateParams;
+        vm.isTeacher=isTeacher;
+        vm.editTeacher=editTeacher;
+
+        fetchAllTeachers();
+
+        function fetchAllTeachers(){
+            var promises = [getTeachers()];
+            return $q.all(promises).then(function() {
+                logger.info('Activated Teachers View');
+            });
+        }
+        function getTeachers() {
+            return teacherservice.getTeachers().then(function(data) {
+                vm.teachers = data._embedded.users;
+                console.log("---------------teachers----------------");
+                console.log(vm.teachers);
+                return vm.teachers;
+            });
+        }
+
+        function isTeacher(currentTeacher) {
+            var role;
+            var isTeach;
+            var roles=currentTeacher.roles
+            var len = roles.length;
+            var i;
+            for (i=0; i<len; i++) {
+                if (i in roles) {
+                    role = roles[i];
+                    if (role == vm.roleName.toUpperCase()) return true;
+                }
+            }
+            return false;
+        }
+
+
+        function editTeacher (teacher ) {
+            console.log('editTeacher pressed. Teacher: ' + teacher.fullName);
+            console.log('editTeacher pressed. $state: ' + $state);
+            console.log('editTeacher pressed. vm.state: ' + vm.state);
+
+            vm.editTeacher.fullName = (teacher.fullName);
+            console.log('editTeacher pressed. vm.editTeacher.fullName: ' + vm.editTeacher.fullName);
+            vm.state.go('add'+vm.roleName,{'teacher': teacher});
+        };
+    }
+})();
