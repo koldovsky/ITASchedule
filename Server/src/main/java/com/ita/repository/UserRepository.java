98c62b647adb5381e9b42aaebf5b3e6004424831
seliuchenko <m.seliuchenko@gmail.com>
2016-10-13 23:47:49 +0300

    Refactored ITAController, Removed ITA service and redundant exceptions

--- Server/src/main/java/com/ita/repository/UserRepository.java
+++ Server/src/main/java/com/ita/repository/UserRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RepositoryRestResource(path = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
 
     public User findByFullName(String fullName);
+    public List<User> findByFullNameIn(List<String> fullNames);
 
 }
