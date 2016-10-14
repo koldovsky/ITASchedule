315994c3f8e7017e8a3e4dc6c531b6b6bf1eb25c
Sviatko <sviat.dub@gmail.com>
2016-10-14 12:10:10 +0300

    sviatko - user controller

--- Server/src/main/java/com/ita/controller/UsersController.java
+++ Server/src/main/java/com/ita/controller/UsersController.java
@@ -5,8 +5,9 @@ import com.ita.entity.User;
 import com.ita.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +19,34 @@ import java.util.List;
  * Created by sdub on 13.10.2016.
  */
 @RestController
+@RequestMapping(value="/users")
 public class UsersController {
     @Autowired
     UserRepository userRepository;
 
-    @RequestMapping(value="/users/{role}", method = RequestMethod.GET)
-    public ResponseEntity<List<User>> getUsers(@RequestBody Role role){
-        List<User> users = new ArrayList<>();
-                userRepository.findByIsActiveTrue();
+    @RequestMapping(value="/roles/{r}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<User>> getUsers(@PathVariable String r){
+        List<Role> roles = new ArrayList<>();
+        try {
+            Role role = Role.valueOf(r);
+            roles.add(role);
+        }
+        catch (IllegalArgumentException e)
+        {
+            return new ResponseEntity<List<User>>(HttpStatus.BAD_REQUEST);
+        }
+        List<User> users = userRepository.findByRoles(roles);
+        if(users.isEmpty()){
+            return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<User>> getUsers(){
+        List<Role> r = new ArrayList<>();
+        r.add(Role.ADMINISTRATOR);
+        List<User> users = userRepository.findByRoles(r);
         if(users.isEmpty()){
             return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
         }
--- Server/src/main/java/com/ita/repository/UserRepository.java
+++ Server/src/main/java/com/ita/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.ita.repository;
 
+import com.ita.entity.Role;
 import com.ita.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
@@ -18,8 +20,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @RestResource(path = "activeusers", rel = "names")
     public List<User> findByIsActiveTrue();
 
-/*    @Query("SELECT user FROM User user JOIN FETCH user_roles ur ON user.id = role. where user.role= (:role) ")
-    @RestResource(path = "findByRole", rel = "findByRole")
-    public List<User> findByRoles (Role role);*/
+    @RestResource(path = "findbyroles", rel = "findbyroles")
+    public List<User> findByRoles(@Param("roles") List<Role> roles);
 
 }
