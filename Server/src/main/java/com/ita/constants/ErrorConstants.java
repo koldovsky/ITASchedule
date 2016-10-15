90f1f50c2de909b88df165ea2e1ed51ba0a6e047
levkovych67 <fnx@ukr.net>
2016-10-15 18:21:18 +0300

    added beta event validation

--- Server/src/main/java/com/ita/constants/ErrorConstants.java
+++ Server/src/main/java/com/ita/constants/ErrorConstants.java
@@ -18,7 +18,11 @@ public final class ErrorConstants {
     public static final String NO_CREATOR_SPECIFIED = "Can't find the specified creator";
     public static final String NO_USERS_SPECIFIED = "List of the teachers is not valid";
     public static final String BLANK_TITLE = "Group must have a title";
-
     public static final String FULLNAME_CANT_BE_EMPTY = "Name can't be empty";
     public static final String EMAIL_IS_NOT_VALID = "Email is not valid";
+    public static final String MISSING_USER = "Missing user in data base";
+    public static final String MISIING_ITAGROUP = "Missing ITA Group in data base";
+    public static final String MISIING_TYPE= "Missing event type ";
+    public static final String MISSING_ROOM= "Missing room";
+
 }
