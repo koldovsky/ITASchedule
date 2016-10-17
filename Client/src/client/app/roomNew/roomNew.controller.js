73cb69f6d41c12a0979665fd1fed7caf3a59c115
dimabush <dim.bush.ua@gmail.com>
2016-10-17 03:54:31 +0300

    add unique filter

--- Client/src/client/app/roomNew/roomNew.controller.js
+++ Client/src/client/app/roomNew/roomNew.controller.js
@@ -3,7 +3,8 @@
 
     angular
         .module('app.roomNew')
-        .controller('roomNewListController', roomNewListController);
+        .controller('roomNewListController', roomNewListController)
+        .filter('unique',unique);
 
     roomNewListController.$inject = ['$q', 'roomNewService', 'logger', '$stateParams', '$state'];
     function roomNewListController($q, roomNewService, logger, $stateParams, $state) {
@@ -67,4 +68,44 @@
         }
 
     }
+    //================filter=======================
+    function unique() {
+        return function (items, filterOn) {
+
+            if (filterOn === false) {
+                return items;
+            }
+
+            if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+                var hashCheck = {}, newItems = [];
+
+                var extractValueToCompare = function (item) {
+                    if (angular.isObject(item) && angular.isString(filterOn)) {
+                        return item[filterOn];
+                    } else {
+                        return item;
+                    }
+                };
+
+                angular.forEach(items, function (item) {
+                    var valueToCheck, isDuplicate = false;
+
+                    for (var i = 0; i < newItems.length; i++) {
+                        if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+                            isDuplicate = true;
+                            break;
+                        }
+                    }
+                    if (!isDuplicate) {
+                        newItems.push(item);
+                    }
+
+                });
+                items = newItems;
+            }
+            return items;
+        };
+        
+    }
+
 })();
