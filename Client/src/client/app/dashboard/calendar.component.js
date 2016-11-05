db9c2c8effb8bb13f3ef6f314405603519cdf970
matsoRuslan <rulya.skillerone@gmail.com>
2016-11-05 20:45:50 +0200

    applied filters

--- Client/src/client/app/dashboard/calendar.component.js
+++ Client/src/client/app/dashboard/calendar.component.js
@@ -1,128 +1,135 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('app.dashboard')
         .component('calendarComp', {
-            bindings   : {
+            bindings: {
                 rooms: '=rooms',
                 teachers: '=teachers',
-                groups: '=groups',
+                groups: '=groups'
             },
             controller: calendarController222,
             templateUrl: '/app/dashboard/calendar.html'
         });
 
-    function calendarController222(logger,$scope,$compile,uiCalendarConfig, $q, eventService, $state) {
+    function calendarController222(logger, $scope, $compile, uiCalendarConfig, $q, eventService, $state, $http, exception) {
         var vm = this;
         vm.title = 'Calendar';
         vm.state = $state;
         vm.filter = filter;
+        vm.createEvent = createEvent;
         activate();
+
         function activate() {
             logger.info('Activated Calendar View');
         }
-
         /* modal window */
         $scope.showTabDialog = function (ev) {
 
-            vm.state.go('editEvent',{"eventToEdit": ev});
+            vm.state.go('editEvent', {"eventToEdit": ev});
         };
-
         /* fetching events from database */
-        $scope.events=[];
-        getEvents().then(function(result) {
-            for (var i in result){
-                $scope.events.push({title: result[i].title,
-                    start: result[i].startTime,
-                    end:result[i].endTime,
-                    stick: true, eventType:result[i].eventType,
-                    itagroups:result[i].itagroups,
-                    users:result[i].users, cityName:result[i].cityName,
-                    addressCodeName:result[i].addressCodeName, roomNumber:result[i].roomNumber,
-                    id:result[i].id, startTime:result[i].startTime, endTime:result[i].endTime });}
-            for (var i in $scope.events) {
-                for (var j in $scope.events[i].itagroups)
-            {console.log($scope.events[i].itagroups[j].title)}}
-        });
-
+        $scope.events = [];
+        filter();
         function getEvents() {
-            return eventService.getEvents().then(function(data) {
+            return eventService.getEvents().then(function (data) {
                 vm.events = data._embedded.events;
                 return vm.events;
             });
         }
-
-
+        function fillArray(array, result, iterator) {
+            array.push({
+                title: result[iterator].title,
+                start: result[iterator].startTime,
+                end: result[iterator].endTime,
+                stick: true, eventType: result[iterator].eventType,
+                itagroups: result[iterator].itagroups,
+                users: result[iterator].users, cityName: result[iterator].cityName,
+                addressCodeName: result[iterator].addressCodeName, roomNumber: result[iterator].roomNumber,
+                id: result[iterator].id, startTime: result[iterator].startTime, endTime: result[iterator].endTime
+            });
+        }
         function filter() {
+
             while ($scope.events.length !== 0) {
                 $scope.events.pop();
             }
-            getEvents().then(function(result) {
-                for (var j in vm.rooms){
-                for (var i in result){
-                    if (result[i].roomNumber === vm.rooms[j].name){
-                        $scope.events.push({title: result[i].title,
-                            start: result[i].startTime,
-                            end:result[i].endTime,
-                            stick: true, eventType:result[i].eventType,
-                            itagroups:result[i].itagroups,
-                            users:result[i].users, cityName:result[i].cityName,
-                            addressCodeName:result[i].addressCodeName, roomNumber:result[i].roomNumber,
-                            id:result[i].id, startTime:result[i].startTime, endTime:result[i].endTime });}}}
-                for (var j in vm.teachers){
-                    for (var i in result){
-                        for (var k in result[i].users)
-
-                    {
-                        if (result[i].users[k].fullName === vm.teachers[j].name){
-                            $scope.events.push({title: result[i].title,
-                                start: result[i].startTime,
-                                end:result[i].endTime,
-                                stick: true, eventType:result[i].eventType,
-                                itagroups:result[i].itagroups,
-                                users:result[i].users, cityName:result[i].cityName,
-                                addressCodeName:result[i].addressCodeName, roomNumber:result[i].roomNumber,
-                                id:result[i].id, startTime:result[i].startTime, endTime:result[i].endTime });}}}}
-                for (var j in vm.groups){
-                    for (var i in result){
-                        for (var k in result[i].itagroups)
-
-                        {
-                            if (result[i].itagroups[k].title === vm.groups[j].name){
-                                $scope.events.push({title: result[i].title,
-                                    start: result[i].startTime,
-                                    end:result[i].endTime,
-                                    stick: true, eventType:result[i].eventType,
-                                    itagroups:result[i].itagroups,
-                                    users:result[i].users, cityName:result[i].cityName,
-                                    addressCodeName:result[i].addressCodeName, roomNumber:result[i].roomNumber,
-                                    id:result[i].id, startTime:result[i].startTime, endTime:result[i].endTime });}}}}
-
-                for (var i in $scope.events) {console.log("id= "+$scope.events[i].id);}
-                for (var i=$scope.events.length-1; i>=0; i--){console.log("i= "+i);
-                    for (j=i-1; j>=0; j--){console.log("j= "+j);if($scope.events[j].id===$scope.events[i].id){
-                        $scope.events.splice(j,1);
-                    }}
+            var filterArray = [];
+            getEvents().then(function (result) {
+                if (vm.rooms.length===0 && vm.teachers.length===0 && vm.groups.length===0){
+                    for (var i in result) {fillArray(filterArray, result, i);}
                 }
+                for (var j in vm.rooms) {
+                    for (var i in result) {
+                        if (result[i].roomNumber === vm.rooms[j].name) {
+                            fillArray(filterArray, result, i);
+                        }
+                    }
+                }
+                for (var j in vm.teachers) {
+                    for (var i in result) {
+                        for (var k in result[i].users) {
+                            if (result[i].users[k].fullName === vm.teachers[j].name) {
+                                fillArray(filterArray, result, i);
+                            }
+                        }
+                    }
+                }
+                for (var j in vm.groups) {
+                    for (var i in result) {
+                        for (var k in result[i].itagroups) {
+                            if (result[i].itagroups[k].title === vm.groups[j].name) {
+                                fillArray(filterArray, result, i);
+                            }
+                        }
+                    }
+                }
+
+                angular.forEach(filterArray, function (value) {
+                    if (containsId($scope.events, value) === false) {
+                        $scope.events.push(value)
+                    }
+                });
 
             });
         }
+        function containsId (list, obj){
+            for (var i = 0; i < list.length; i++) {
+                if (list[i].id === obj.id) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        function createEvent(){vm.state.go('editEvent', {"eventToEdit": null});}
         /* config object */
         $scope.uiConfig = {
-            calendar:{
-                header:{
+            calendar: {
+                header: {
                     left: 'month agendaWeek agendaDay',
                     center: 'title',
                     right: 'today prev,next'
                 },
-                eventClick: function(event) {
+                defaultView: 'agendaWeek',
+                displayEventTime: false,
+                eventClick: function (event) {
 
-                    $scope.showTabDialog(event);
+                    vm.state.go('editEvent', {"eventToEdit": event});
 
                 },
-                dayClick: function (date){
-                    console.log(vm.rooms[0].name);
+                dayClick: function (date) {
+                    var event = {
+                        title: null,
+                        stick: true, eventType: null,
+                        itagroups: [],
+                        users: [], cityName: null,
+                        addressCodeName: null, roomNumber: null,
+                        startTime:  date, endTime:date
+                    };
+                    vm.state.go('editEvent', {"eventToEdit": event});
+
                 },
                 eventDrop: $scope.alertOnDrop,
                 eventResize: $scope.alertOnResize,
